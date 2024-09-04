a94715c64b7efe7b978ee0260626f229132daed8
unknown <vaibhavpratham507@gmail.com>
2024-09-04 19:11:11 +0530

    Added Middleware and Authorization,Contact API routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator')
 
 const User = require('../models/User');
@@ -16,10 +18,12 @@ router.post('/',
         check('password', "Please enter a password with 6 or more characters").isLength({ min: 6 }),
     ],
     async (req, res) => {
+        
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
         const { name, email, password } = req.body;
 
         try {
@@ -41,7 +45,19 @@ router.post('/',
 
             await user.save();
 
-            res.send("User saved")
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get("jwtSecret"), {
+                expiresIn: 360000
+            }, (err, token)=> {
+                if (err) 
+                    throw err;
+                res.json({ token });
+            });
 
         } catch (err) {
             console.error(err.message);
